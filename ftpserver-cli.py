898348acfc6ff282667012f56401ec3ae900d182
valerio <notisset@gmail.com>
2016-06-26 19:32:21 +0200

    reformat

--- ftpserver-cli.py
+++ ftpserver-cli.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-#title           :ftpserver-cli.py
-#description     :CLI ftp server.
-#author          :Stefano Palazzo
-#usage           :python ftpserver-cli.py --directory=/tmp/srvtest
-#python_version  :2.7.9
-#==============================================================================
+# title           :ftpserver-cli.py
+# description     :CLI ftp server.
+# author          :Stefano Palazzo
+# usage           :python ftpserver-cli.py --directory=/tmp/srvtest
+# python_version  :2.7.9
+# ==============================================================================
 
 import sys
 # sys.path.append("/path/to/pyftpdlib-svn") # enter your proper path here
@@ -14,29 +14,31 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+
 def processCmdLineOptions():
-  global optparser
-  optparser = argparse.ArgumentParser(description="ftpserver-cli",
-              formatter_class=argparse.RawDescriptionHelpFormatter)
-  optparser.add_argument('-u', '--username', action='store', type=str,
-      default="user", help="username")
-  optparser.add_argument('-p', '--password', action='store', type=str,
-      default="12345", help="password")
-  optparser.add_argument('-t', '--port', action='store', type=int,
-      default="21", help="port")
-  optparser.add_argument('-d', '--directory', action='store', type=str,
-      default="/home/user/Projects/", help="port")
-  optargs = optparser.parse_args(sys.argv[1:]) #(sys.argv)
-  return optargs
+    global optparser
+    optparser = argparse.ArgumentParser(description="ftpserver-cli",
+                                        formatter_class=argparse.RawDescriptionHelpFormatter)
+    optparser.add_argument('-u', '--username', action='store', type=str,
+                           default="user", help="username")
+    optparser.add_argument('-p', '--password', action='store', type=str,
+                           default="12345", help="password")
+    optparser.add_argument('-t', '--port', action='store', type=int,
+                           default="21", help="port")
+    optparser.add_argument('-d', '--directory', action='store', type=str,
+                           default="/home/user/Projects/", help="port")
+    optargs = optparser.parse_args(sys.argv[1:])  # (sys.argv)
+    return optargs
 
 
 optargs = processCmdLineOptions()
 
-print("Using: user: %s pass: %s port: %d dir: %s" % (optargs.username, optargs.password, optargs.port, optargs.directory))
+print(
+    "Using: user: %s pass: %s port: %d dir: %s" % (optargs.username, optargs.password, optargs.port, optargs.directory))
 
 authorizer = DummyAuthorizer()
 authorizer.add_user(optargs.username, optargs.password, optargs.directory, perm="elradfmw")
-#authorizer.add_anonymous("/home/nobody")
+# authorizer.add_anonymous("/home/nobody")
 
 handler = FTPHandler
 handler.authorizer = authorizer
